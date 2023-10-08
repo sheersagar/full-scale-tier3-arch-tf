681a710a05234be4d965d4136f221bebd52a3da5
fireflycons <34012094+fireflycons@users.noreply.github.com>
2023-10-08 20:36:20 +0100

    Optimize subnet creation

--- subents.tf
+++ subents.tf
@@ -1,96 +1,70 @@
 
-# Use data source to get all availability zones in region
-data "aws_availability_zones" "available_zones" {}
+# Use data source to get first 3 availability zones in region
+data "aws_availability_zones" "available_zones" {
+  filter {
+    name = "zone-name"
+    values = [
+      "${var.region}a",
+      "${var.region}b",
+      "${var.region}c"
+    ]
+  }
+}
 
+# Generate CIDR ranges for all the subnetsw
+locals {
+  cidr_ranges_pub_sn = {
+    "${var.region}a" = cidrsubnet("10.16.0.0/16", 8, 0)
+    "${var.region}b" = cidrsubnet("10.16.0.0/16", 8, 1)
+    "${var.region}c" = cidrsubnet("10.16.0.0/16", 8, 2)
+  }
 
+  cidr_ranges_pvt_sn = {
+    "${var.region}a" = cidrsubnet("10.16.0.0/16", 8, 3)
+    "${var.region}b" = cidrsubnet("10.16.0.0/16", 8, 4)
+    "${var.region}c" = cidrsubnet("10.16.0.0/16", 8, 5)
+  }
+
+  cidr_ranges_db_sn = {
+    "${var.region}a" = cidrsubnet("10.16.0.0/16", 8, 6)
+    "${var.region}b" = cidrsubnet("10.16.0.0/16", 8, 7)
+    "${var.region}c" = cidrsubnet("10.16.0.0/16", 8, 8)
+  }
+}
 
 #############################Public Subnet##################################
 #############################             ##################################
 
 
-# Creating public subnet in AZ1
-resource "aws_subnet" "pb_sn_az1" {
+# Creating public subnets in AZ1
+resource "aws_subnet" "pb_sn" {
+  for_each = toset(data.aws_availability_zones.available_zones.names)
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pb_sn_az1_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  cidr_block              = local.cidr_ranges_pub_sn[each.key]
+  availability_zone       = each.key
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "pb-sn-az1"
+    Name = "pb-sn-${each.key}"
   }
 }
-
-# Creating public subnet in AZ2
-resource "aws_subnet" "pb_sn_az2" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pb_sn_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "pb-sn-az2"
-  }
-}
-
-
-# creating public subnet in AZ3
-resource "aws_subnet" "pb_sn_az3" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pb_sn_az3_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[2]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "pb-sn-az3"
-  }
-}
-
-
-
-
-
 
 
 ########################App Private Subnet##################################
 ########################                  ##################################
 
-# Creating Private app subnet in AZ1
+# Creating Private app subnets in AZ1
 resource "aws_subnet" "pvt_sn_app_az1" {
+  for_each = toset(data.aws_availability_zones.available_zones.names)
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pvt_sn_app_az1_cidr
+  cidr_block              = local.cidr_ranges_pvt_sn[each.key]
   availability_zone       = data.aws_availability_zones.available_zones.names[0]
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "pvt-app-sn-az1"
+    Name = "pvt-app-sn-${each.key}"
   }
 }
-
-# Creating Private app subnet in AZ2
-resource "aws_subnet" "pvt_sn_app_az2" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pvt_sn_app_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1]
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "pvt-app-sn-az2"
-  }
-}
-
-
-# creating private app subnet in AZ3
-resource "aws_subnet" "pvt_sn_app_az3" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pvt_sn_app_az3_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[2]
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "pvt-app-sn-az3"
-  }
-}
-
 
 #---------------------------------------------------------------
 
@@ -99,36 +73,13 @@ resource "aws_subnet" "pvt_sn_app_az3" {
 
 # creating private db subnet in AZ1
 resource "aws_subnet" "pvt_sn_db_az1" {
+  for_each = toset(data.aws_availability_zones.available_zones.names)
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pvt_sn_db_az1_cidr
+  cidr_block              = local.cidr_ranges_db_sn[each.key]
   availability_zone       = data.aws_availability_zones.available_zones.names[0]
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "pvt-app-db-az1"
-  }
-}
-
-# creating private db subnet in AZ2
-resource "aws_subnet" "pvt_sn_db_az2" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pvt_sn_db_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1]
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "pvt-app-db-az2"
-  }
-}
-
-# creating private db subnet in AZ3
-resource "aws_subnet" "pvt_sn_db_az3" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = var.pvt_sn_db_az3_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[2]
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "pvt-app-db-az3"
+    Name = "pvt-app-db-${each.key}"
   }
 }
